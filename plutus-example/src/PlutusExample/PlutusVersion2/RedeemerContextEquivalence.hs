a9763f89f65ec4c17b11a40de37a4824c3a7a201
Jordan Millar <jordan.millar@iohk.io>
2022-06-23 12:18:54 -0500

    WIP

--- plutus-example/src/PlutusExample/PlutusVersion2/RedeemerContextEquivalence.hs
+++ plutus-example/src/PlutusExample/PlutusVersion2/RedeemerContextEquivalence.hs
@@ -6,18 +6,20 @@
 
 
 module PlutusExample.PlutusVersion2.RedeemerContextEquivalence
-  ( v2ScriptContextEquivalenceScript
+  ( PV2CustomRedeemer (..)
+  , v2ScriptContextEquivalenceScript
   , v2ScriptContextEquivalenceSbs
   ) where
 
 import Prelude hiding (($))
 
-import Cardano.Api.Shelley (PlutusScript (..), PlutusScriptV2)
+import Cardano.Api.Shelley
 import Prelude hiding (($), (&&))
 
 import Codec.Serialise
 import Data.ByteString.Lazy qualified as LBS
 import Data.ByteString.Short qualified as SBS
+
 
 import Plutus.Script.Utils.V2.Scripts.Validators as V2
 import Plutus.V2.Ledger.Api qualified as V2
@@ -40,7 +42,7 @@ data PV2CustomRedeemer
       , pv2Signatories :: [V2.PubKeyHash]
       , pv2Redeemers   :: V2.Map ScriptPurpose V2.Redeemer
       , pv2Data        :: V2.Map V2.DatumHash V2.Datum
-      }
+      } deriving (Prelude.Eq, Show)
 
 PlutusTx.unstableMakeIsData ''MyCustomDatumV2
 PlutusTx.unstableMakeIsData ''PV2CustomRedeemer
@@ -50,7 +52,18 @@ PlutusTx.unstableMakeIsData ''PV2CustomRedeemer
 {-# INLINABLE mkValidator #-}
 mkValidator :: MyCustomDatumV2 -> PV2CustomRedeemer -> V2.ScriptContext -> Bool
 mkValidator _ redeemer scriptContext =
-  inputsAreEquivalent redeemer txInfo
+  -- These all work fine
+  --inputsAreEquivalent redeemer txInfo PlutusPrelude.&&
+  --referenceInputsAreEquivalent redeemer txInfo PlutusPrelude.&&
+  --certsAreEquivalent redeemer txInfo PlutusPrelude.&&
+  --reqSignersAreEquivalent redeemer txInfo PlutusPrelude.&&
+  --datumHashMapsAreEquivalent redeemer txInfo
+  -- These below are failing
+  validtyIntervalsAreEquivalent redeemer txInfo
+  -- TODO: Tx outs appear to not be working
+  -- outputsAreEquivalent redeemer txInfo
+  -- TODO: Validity interval equivalence appears to not be working
+  -- TODO: You can't check the fee with the build command due to how it's constructed
  where
   txInfo :: V2.TxInfo
   txInfo = V2.scriptContextTxInfo scriptContext
@@ -58,7 +71,36 @@ mkValidator _ redeemer scriptContext =
   inputsAreEquivalent :: PV2CustomRedeemer -> V2.TxInfo -> Bool
   inputsAreEquivalent (PV2CustomRedeemer inputs _ _ _ _ _ _ _ _ _ _) tInfo =
     (PlutusPrelude.map txInInfoResolved $ V2.txInfoInputs tInfo) PlutusPrelude.==
-    (PlutusPrelude.map txInInfoResolved inputs)
+    PlutusPrelude.map txInInfoResolved inputs
+
+  referenceInputsAreEquivalent :: PV2CustomRedeemer -> V2.TxInfo -> Bool
+  referenceInputsAreEquivalent (PV2CustomRedeemer _ refInputs _ _ _ _ _ _ _ _ _) tInfo =
+    (PlutusPrelude.map txInInfoResolved $ V2.txInfoReferenceInputs tInfo) PlutusPrelude.==
+    PlutusPrelude.map txInInfoResolved refInputs
+
+  _outputsAreEquivalent :: PV2CustomRedeemer -> V2.TxInfo -> Bool
+  _outputsAreEquivalent (PV2CustomRedeemer _ _ outputs _ _ _ _ _ _ _ _) tInfo =
+    V2.txInfoOutputs tInfo PlutusPrelude.== outputs
+
+  _feesAreEquivalent :: PV2CustomRedeemer -> V2.TxInfo -> Bool
+  _feesAreEquivalent  (PV2CustomRedeemer _ _ _ fee _ _ _ _ _ _ _) tInfo =
+    V2.txInfoFee tInfo PlutusPrelude.== fee
+
+  certsAreEquivalent :: PV2CustomRedeemer -> V2.TxInfo -> Bool
+  certsAreEquivalent (PV2CustomRedeemer _ _ _ _ _ certs _ _ _ _ _) tInfo =
+    V2.txInfoDCert tInfo PlutusPrelude.== certs
+
+  validtyIntervalsAreEquivalent :: PV2CustomRedeemer -> V2.TxInfo -> Bool
+  validtyIntervalsAreEquivalent (PV2CustomRedeemer _ _ _ _ _ _ _ validInterval _ _ _) tInfo =
+    V2.txInfoValidRange tInfo PlutusPrelude.== validInterval
+
+  reqSignersAreEquivalent :: PV2CustomRedeemer -> V2.TxInfo -> Bool
+  reqSignersAreEquivalent (PV2CustomRedeemer _ _ _ _ _ _ _ _ reqSigners _ _) tInfo =
+    V2.txInfoSignatories tInfo PlutusPrelude.== reqSigners
+
+  datumHashMapsAreEquivalent :: PV2CustomRedeemer -> V2.TxInfo -> Bool
+  datumHashMapsAreEquivalent (PV2CustomRedeemer _ _ _ _ _ _ _ _ _ _ datumHashMap) tInfo =
+    V2.txInfoData tInfo PlutusPrelude.== datumHashMap
 
 validator :: V2.Validator
 validator = V2.mkValidatorScript
@@ -75,6 +117,7 @@ v2ScriptContextEquivalenceSbs =
 
 v2ScriptContextEquivalenceScript :: PlutusScript PlutusScriptV2
 v2ScriptContextEquivalenceScript = PlutusScriptSerialised v2ScriptContextEquivalenceSbs
+
 {-
       txInfoInputs          :: [TxInInfo] -- ^ Transaction inputs
     , txInfoReferenceInputs :: [TxInInfo] -- ^ Transaction reference inputs
